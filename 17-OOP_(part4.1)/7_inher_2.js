c491e72b30ceb363ac7ae260ba88754622a4b82b
Himanshu Chandel <147255528+Himanshuuu-exe@users.noreply.github.com>
2025-09-08 22:34:41 +0530

    Add files via upload
    Day 17: Explored constructors vs object literals, property access in unrelated objects, and used instanceof to check object inheritance in JS.

---
+++ 17-OOP_(part4.1)/7_inher_2.js
@@ -0,0 +1,16 @@
+function rectangle()
+{
+    this.length=0;
+    this.widht=0;
+}
+
+function box()
+{
+    this.height=0;
+}
+
+var a=new box();
+console.log(a.height);
+console.log(a.widht);
+console.log(a.length);
+
---
+++ 17-OOP_(part4.1)/8_inheri_3.js
@@ -0,0 +1,20 @@
+function rectangle()
+{
+    this.length=0;
+    this.width=0;
+}
+
+function box()
+{
+    this.height=0;
+}
+
+var a=new box();
+console.log(a.height);
+
+a.length=0;
+console.log(a.length);
+
+a.width=0;
+console.log(a.width);
+
