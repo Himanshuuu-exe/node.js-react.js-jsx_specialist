ef6bc7283e2fc234f32d346e25a3c765a8207213
Himanshu Chandel <147255528+Himanshuuu-exe@users.noreply.github.com>
2025-09-18 22:54:18 +0530

    Add files via upload
    Day 27 → Practiced class expressions by assigning classes to variables. Compared named vs unnamed versions and created objects from both.

---
+++ 27-OOP_(part6.2)/4.js
@@ -0,0 +1,11 @@
+a=class{
+    constructor()
+    {
+        this.x=10;
+        this.y=20;
+    }
+}
+
+console.log(a);
+b=new a();
+console.log(b);
---
+++ 27-OOP_(part6.2)/5.js
@@ -0,0 +1,12 @@
+a=class abcd{
+    constructor()
+    {
+        this.x=10;
+        this.y=20;
+    }
+}
+
+console.log(a);
+b=new a();
+console.log(b);
+var c=new abcd();
