a36cf5215d85693e484cf9e1e88fb7683584e6aa
Himanshu Chandel <147255528+Himanshuuu-exe@users.noreply.github.com>
2025-10-30 23:56:42 +0530

    commit success
    Implemented monitor.js — a monitoring client that listens to chat server events (login/logout/user list) using net, events, and custom actions.

---
+++ 69-cli_chat_application_1.3/monitor.js
@@ -0,0 +1,95 @@
+const net=require('net');
+const events=require('events');
+
+class DataModel
+{
+    constructor(){
+    this.id=null;
+    this.username=null;
+    }
+}
+
+class Request
+{
+    constructor()
+    {
+        this.action="";
+    }
+}
+
+var model=new DataModel();
+var eventEmitter=new events.EventEmitter();
+var client=null;
+
+//events 
+
+function loggedOut()
+{
+    console.log(`User ${model.username} Logged Out From Here...`);
+    process.exit(0);
+}
+
+function usersListArrived(users)
+{
+    console.log("List Of Online Users");
+    for(var e=0; e<users.length; e++)
+        {
+            console.log(users[e]);
+        }    
+}
+
+function monitorCreated(username)
+{
+    model.username=username;
+    console.log("This Is The Monitor For User :->"+username);
+}
+
+function monitorDenied()
+{
+    console.log("Unable to create monitor :-("+model.id);
+    process.exit(0);
+}
+
+//setting up events 
+
+eventEmitter.on('loggedOut',loggedOut);
+eventEmitter.on('usersListArrived',usersListArrived);
+eventEmitter.on('monitorCreated',monitorCreated);
+eventEmitter.on('monitorDenied',monitorDenied);
+
+model.id=process.argv[2];
+client=new net.Socket();
+client.connect(2189,"localhost",function(){
+    console.log("connected to chat server");
+    let request=new Request();
+    request.action="createMonitor";
+    request.userID=model.id;
+    client.write(JSON.stringify(request));    
+});
+
+client.on('data',function(data){
+    var response=JSON.parse(data);
+    
+    if(response.action=="createMonitor")
+    {
+        if(response.result != null && response.result.length>0)
+        {
+            eventEmitter.emit('monitorCreated',response.result);
+        }
+        else
+        {
+            eventEmitter.emit('monitorDenied')
+        }
+    }
+
+    if(response.action=="logout") eventEmitter.emit('loggedOut');
+    if(response.action=="getUsers") eventEmitter.emit('usersListArrived',response.result);
+});
+
+client.on('end',function(){
+    console.log("Server Ended The Connection");
+});
+
+client.on('error',function(){
+    console.log("some problem occured at server side");
+});
